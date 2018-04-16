1a8047bfcab4bd6807c2a379b011acb2cb68a76e
Elkana Orbach <optimove.develop.mobile@gmail.com>
2018-04-16 10:55:04 +0300

    Add 'Codable' conformance for Event properties in order to enable saving to file

--- OptiTrackCore/Classes/Event.swift
+++ OptiTrackCore/Classes/Event.swift
@@ -25,7 +25,7 @@ import CoreGraphics
 /// - idsite, rec, rand, apiv, res, cookie,
 /// - All Plugins: fla, java, dir, qt, realp, pdf, wma, gears, ag
 /// - cid: We will use the uid instead of the cid.
-public struct Event {
+public struct Event: Codable {
     let siteId: String
     public let uuid: NSUUID
     let visitor: Visitor
@@ -54,7 +54,7 @@ public struct Event {
     /// Currently only used for Campaigns
     /// api-key: urlref
     let referer: URL?
-    let screenResolution : CGSize = Device.makeCurrentDevice().screenSize
+    var screenResolution : CGSize = Device.makeCurrentDevice().screenSize
     
     /// api-key: _cvar
     let customVariables: [CustomVariable]
@@ -69,6 +69,84 @@ public struct Event {
     let dimensions: [CustomDimension]
     
     let customTrackingParameters: [String:String]
+    
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: JsonCodingKeys.self)
+        siteId = try values.decode(String.self, forKey: .siteId)
+        uuid = NSUUID(uuidString: try values.decode(String.self, forKey: .uuid))!
+        visitor = try values.decode(Visitor.self, forKey: .visitor)
+        session = try values.decode(Session.self, forKey: .session)
+        date = try values.decode(Date.self, forKey: .date)
+        
+        if let urlPath = try? values.decode(String.self, forKey: .url).removingPercentEncoding {
+            url = URL(string: urlPath!)
+        } else {
+            url = nil
+        }
+        actionName = try values.decode([String].self, forKey: .actionName)
+        language = try values.decode(String.self, forKey: .language)
+        isNewSession = try values.decode(Bool.self, forKey: .isNewSession)
+        if let urlPath = try? values.decode(String.self, forKey: .referer).removingPercentEncoding {
+            referer = URL(string: urlPath!)
+        } else {
+            referer = nil
+        }
+        screenResolution  = try values.decode(CGSize.self, forKey: .screenResolution)
+        customVariables = try values.decode([CustomVariable].self, forKey: .customVariables)
+        eventCategory = try? values.decode(String.self, forKey: .eventCategory)
+        eventAction = try? values.decode(String.self, forKey: .eventAction)
+        eventName = try? values.decode(String.self, forKey: .eventName)
+        eventValue = try? values.decode(Float.self, forKey: .eventValue)
+        dimensions = try values.decode([CustomDimension].self, forKey: .dimensions)
+        customTrackingParameters = try values.decode([String:String].self, forKey: .customTrackingParameters)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: JsonCodingKeys.self)
+        try container.encode(siteId, forKey: .siteId)
+        try container.encode(uuid.uuidString, forKey: .uuid)
+        try container.encode(visitor, forKey: .visitor)
+        try container.encode(session, forKey: .session)
+        try container.encode(date, forKey: .date)
+        try container.encode(url, forKey: .url)
+        try container.encode(actionName, forKey: .actionName)
+        try container.encode(language, forKey: .language)
+        try container.encode(isNewSession, forKey: .isNewSession)
+        try container.encode(referer, forKey: .referer)
+        try container.encode(screenResolution, forKey: .screenResolution)
+        try container.encode(customVariables, forKey: .customVariables)
+        try container.encode(eventCategory, forKey: .eventCategory)
+        try container.encode(eventAction, forKey: .eventAction)
+        try container.encode(eventName, forKey: .eventName)
+        try container.encode(eventValue, forKey: .eventValue)
+        try container.encode(dimensions, forKey: .dimensions)
+        try container.encode(customTrackingParameters, forKey: .customTrackingParameters)
+        
+        
+    }
+    enum JsonCodingKeys: String,CodingKey
+    {
+        case siteId
+        case uuid
+        case visitor
+        case session
+        case date
+        case url
+        case actionName
+        case language
+        case isNewSession
+        case referer
+        case screenResolution
+        case customVariables
+        case eventCategory
+        case eventAction
+        case eventName
+        case eventValue
+        case dimensions
+        case customTrackingParameters
+    }
+    
 }
 
 extension Event {
